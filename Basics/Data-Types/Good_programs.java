df80ea8198587c3cc329b3bdf4238e4c7c535be0
Babul Singh — Learning Systems & Problem Solving. <babulkumarsingh573922@gmail.com>
2026-04-01 23:38:35 +0530

    Add files via upload
    This is Some more Question Solved.

---
+++ Basics/Data-Types/Good_programs.java
@@ -0,0 +1,114 @@
+class Good_programs{
+    public static void main(String[] args){
+
+//    1 Print all primitive data types
+
+        // System.out.println("byte: " + 12);
+        // System.out.println("char: " + 'A');
+        // System.out.println("short: " + 1289);
+        // System.out.println("int: " + 1523289);
+        // System.out.println("long: " + 74631523289453l);
+        // System.out.println("float: " + 7.4631523f);
+        // System.out.println("double: " + 7.46315235444364d);
+        // System.out.println("boolean: " + true);
+
+
+//    2 Print size and range of types
+
+        // System.out.println("byte: Range = -128 to 127");
+        // System.out.println("Short: Range = -32768 to 32767");
+
+
+//    3 Add two bytes
+
+        // byte a = 12;
+        // byte b = 79;
+        // System.out.println(a + b);
+
+
+//    4 Integer division
+
+        // int a = 189;
+        // int b = 8;
+        // System.out.println(a/b);
+
+
+//    5 byte + byte becomes int
+
+        // int a = 127;
+        // int b = 8;
+        // System.out.println(a+b);   // as it gives value beyond byte range so it's int.
+
+
+//    6 byte to short
+
+        // byte b = 125;
+        // short s = (short)b;
+        // System.out.println(s);
+
+
+//    7 int to double
+
+        // int a = 6337557;
+        // double d = (double)a;
+        // System.out.println(d);
+
+
+//    8 double to int
+
+        // double a = 189.87d;
+        // int b = (int)a;
+        // System.out.println(b);
+
+
+//    9 short to byte
+
+        // short a = 18987;
+        // byte b = (byte)a;
+        // System.out.println(b);
+
+
+//   10 123.99 to int
+
+        // double d = 123.99;
+        // int i = (int)d;
+        // System.out.println(i);
+
+
+//   11 130 to byte
+
+        // int a = 130;
+        // byte b = (byte)a;
+        // System.out.println(b);
+
+
+//   12 ASCII value of character
+
+        // char a = 'a';
+        // int b = (int)a;
+        // System.out.println(b);
+
+
+//   13 lowercase to uppercase
+
+        // String s = "cat";
+        // String b = s.toUpperCase();
+        // System.out.println(b);
+
+
+//   14 String to int                   //////                  ????????????????????????????????
+
+        // String s = "123";
+        // int a = Integer.parseInt(s);
+        // System.out.println(a);
+
+
+//   15 Average of 3 integers using casting
+
+        int a = 12;
+        int b = 32;
+        int c = 72;
+        System.out.println((a+b+c)/3);
+
+    }
+}
